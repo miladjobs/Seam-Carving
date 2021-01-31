b78d8f2fae5745b9e020a1509fca99c735556196
Milad Esfandiyari <miladisjobs@gmail.com>
2021-01-31 23:30:26 +0330

    finalize code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from seam import *
 import time
 from PIL import Image
+import argparse
+import sys
 
 global start_time
 
@@ -49,15 +51,50 @@ def create_gif(images, image_with_select_seam, energy_images, energy_images_with
 
 
 if __name__ == '__main__':
-    array = np.array(Image.open('../samples/input/3.png'), dtype=int)
+    parser = argparse.ArgumentParser(description="get input image and vertical& horizontal pixels , get output path")
+    parser.add_argument('-i', type=str)
+    parser.add_argument('-x', type=int)
+    parser.add_argument('-y', type=int)
+    parser.add_argument('-o', type=str)
+    try:
+        args = parser.parse_args()
+    except Exception:
+        print("error with input")
+        sys.exit()
+    input_file_path = args.i
+    vertical_pixels_count = args.x
+    horizontal_pixels_count = args.y
+    output_directory_path = args.o
+    if '/' in input_file_path:
+        input_directory_path = ""
+        input_file_path_split = str(input_file_path).split('/')
+        input_file_name = input_file_path_split[-1].split('.')[0]
+        for i in range(len(input_file_path_split) - 1):
+            input_directory_path += input_file_path_split[i] + "/"
+        if output_directory_path:
+            if not output_directory_path[-1] == '/':
+                output_directory_path += '/'
+    else:
+        input_directory_path = ""
+        input_file_path_split = str(input_file_path).split('\\')
+        input_file_name = input_file_path_split[-1].split('.')[0]
+        for i in range(len(input_file_path_split) - 1):
+            input_directory_path += input_file_path_split[i] + "\\"
+        if output_directory_path:
+            if not output_directory_path[-1] == '\\':
+                output_directory_path += '\\'
+
+    array = np.array(Image.open(input_file_path), dtype=int)
     start_time = time.time()
-    output = seam_carving(array, 10, 10)
+    output = seam_carving(array, int(vertical_pixels_count), int(horizontal_pixels_count))
     t1 = time.time()
     print(f"process finished! elapsed time : {(t1 - start_time): .3f}s")
     print("--------------- \n save output image and gif")
     gif_images = create_gif(output[0], output[1], output[2], output[3])
-    gr_img = Image.fromarray(output[0][-1].astype(np.uint8))
-    gr_img.save('../samples/output/3_out.png')
-    gif_images[0].save('../samples/output/3.gif', save_all=True, append_images=gif_images[1:], duration=100, loop=0,
+    final_image = Image.fromarray(output[0][-1].astype(np.uint8))
+    if not output_directory_path:
+        output_directory_path = input_directory_path
+    final_image.save(output_directory_path+input_file_name+'_out.png')
+    gif_images[0].save(output_directory_path+input_file_name+'_out.gif', save_all=True, append_images=gif_images[1:], duration=100, loop=0,
                        optimize=False)
     print("finished!")
